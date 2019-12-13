edfb914327359f798a6091176c374857a34feee7
Alexey Che <rahnar0ker@gmail.com>
2019-12-13 23:17:54 +0400

    test5

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ document.write("Temp page");
 function showMessage(){
   alert ("click by div");
 }
+
+function areaRectangle(){
+  var a=document.forma1.t1.value;
+  var b=document.forma1.t2.value;
+  var s=a*b;
+  document.forma1.res.value=s;
+}
