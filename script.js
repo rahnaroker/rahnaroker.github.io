30ab6afb5bf1c698921ff0070670f91ec26b9f10
Alexey Che <rahnar0ker@gmail.com>
2019-12-14 00:19:38 +0400

    test5

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function showMessage(){
   alert ("click by div");
 }
 
-function areaRectangle(){
-  var a=document.forma1.t1.value;
-  var b=document.forma1.t2.value;
+function areaRectangle(obj){
+  var a=obj.t1.value;
+  var b=obj.t2.value;
   var s=a*b;
-  document.forma1.res.value=s;
+  obj.res.value=s;
 }
