18d28d4b6720effe03331c6ebef20b02187c3457
Alexey Che <rahnar0ker@gmail.com>
2019-12-21 01:12:24 +0400

    test5

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ function showDesc(obj, n){
 function delet(obj){
   obj.desc.value=' ';
 }
+
+function areaOfTriangle(obj){
+  var a=1*obj.st1.value;
+  var b=1*obj.st2.value;
+  var c=1*obj.st3.value;
+  var p=(a+b+c)/2;
+  var s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+  s=s.toFixed(2);
+  obj.res.value=s;
+}
