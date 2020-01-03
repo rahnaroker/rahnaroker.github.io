64fc4d0c206390ea316f85dac182216e85031dfd
Alexey Che <rahnar0ker@gmail.com>
2020-01-04 00:28:33 +0400

    test5

--- script.js
+++ script.js
@@ -32,3 +32,13 @@ function areaOfTriangle(obj){
   s=s.toFixed(2);
   obj.res.value=s;
 }
+
+function maxZnach(obj){
+  var a=1*obj.zn1.value;
+  var b=1*obj.zn2.value;
+  var c=1*obj.zn3.value;
+  var m=a;
+  if (b>m) m=b;
+  if (c>m) m=c;
+  obj.res.value=m;
+}
